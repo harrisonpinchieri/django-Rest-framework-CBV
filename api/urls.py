82a20ee48fca0fcb81877b93120203636200860c
harrison <harrisonpinchieri@gmail.com>
2024-01-08 04:19:59 -0300

    adicionando matricula na api

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-from api.views import AlunosViewSet, CursosViewSet
+from api.views import AlunosViewSet, CursosViewSet, MatriculasViewSet
 
 router = routers.DefaultRouter()
 router.register("alunos", AlunosViewSet, basename="Alunos")
 router.register("cursos", CursosViewSet, basename="Cursos")
+router.register("matriculas", MatriculasViewSet, basename="Matriculas")
 
 
 urlpatterns = [
